ec29f2811737a0c792cb708538c3ce2bc020dc8e
Shilpa39 <3876alaha@gmail.com>
2018-01-01 19:43:41 +0530

    test feed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,12 +8,32 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
 num:number = 0;
+num1:number = 0;
+fillbu:boolean=false;
+fillbd:boolean=false;
+
   constructor(public navCtrl: NavController) {
 
   }
 upvote()
 {
-this.num=this.num+1;
+if(this.num==0)
+{this.num=1;
+this.fillbu=true;}
+else
+{this.num=0;
+this.fillbu=false;}
+}
+
+downvote()
+{
+if(this.num1==0)
+{this.num1=1;
+this.fillbd=true;
+}
+else
+{this.num1=0;
+this.fillbd=false;}
 }
 
 }
