782b44c407e9ade0f1a025b81df65577b32f2791
Shilpa39 <3876alaha@gmail.com>
2018-01-18 09:14:14 +0530

    coloured

--- src/pages/components/post.component.ts
+++ src/pages/components/post.component.ts
@@ -4,12 +4,12 @@ import { Component, Input } from '@angular/core';
   selector: 'post',
   template: `
 
-    <ion-item>
+    <ion-item style="background-color:#ff3399;z-index:5;">
       <ion-avatar item-start>
         <img [src]="fact.AuthorImage">
       </ion-avatar>
-      <h2>{{fact.Author}}</h2>
-      <p>{{fact.Date}}</p>
+      <h2 style="color:white;text-shadow: 2px 2px 4px #000000;">{{fact.Author}}</h2>
+      <p style="color:white;text-shadow: 2px 2px 4px #000000;">{{fact.Date}}</p>
     </ion-item>
     
     
